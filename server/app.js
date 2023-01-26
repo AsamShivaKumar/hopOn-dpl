0422e5d96f92e1389c8e830fdd9ac7d8656fbd1b
AsamShivaKumar <asamshivakumar@gmail.com>
2023-01-26 12:12:13 +0530

    Setup of basic repo structute and some bolier plate-code

--- server/app.js
+++ server/app.js
@@ -1,6 +1,22 @@
 const exp = require("express");
 const app = exp();
+const mongoose = require("mongoose");
 
+mongoose.connect('mongodb://localhost:27017/hopOnDB');
+
+const driverSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    verified: Boolean,
+    rides: [mongoose.ObjectId],
+    rating: Number
+});
+
+const Driver = new mongoose.model("Driver", driverSchema);
+
+app.get("/app", (req,res) => {
+    res.send("Response from server!")
+})
 
 app.listen(4000, () => {
     console.log("Server started on port 4000")
