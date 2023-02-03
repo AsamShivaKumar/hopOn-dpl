f3c3b0e9f6d71eb34a0ef402ea57f30f3344c413
AsamShivaKumar <asamshivakumar@gmail.com>
2023-02-03 14:43:24 +0530

    Front-end for Verification page completed

--- server/app.js
+++ server/app.js
@@ -1,23 +1,44 @@
 const exp = require("express");
 const app = exp();
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 mongoose.connect('mongodb://localhost:27017/hopOnDB');
+
+app.use(exp.json());
+app.use(exp.urlencoded({extended: true}));
 
 const driverSchema = new mongoose.Schema({
     username: String,
     name: String,
     email: String,
+    mobile: Number,
     password: String,
-    verified: Boolean,
+    verified: { type: Boolean, default: false},
+    docs: {type: Array, default: [2,2,2,2,2,2]},
     rides: Number,
     rating: Number
 });
 
+const dest = "./images";
 const Driver = new mongoose.model("Driver", driverSchema);
 
-app.get("/app", (req,res) => {
-    res.send("Response from server!")
+const storageEngine = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null,dest)
+    },
+    filename: (req,file,cb) => {
+        console.log(req.body);
+        console.log(file.originalname);
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, fileName);
+    }
+});
+
+const upload = multer({storage: storageEngine});
+
+app.post("/upload_file", upload.single('image'),(req,res) => {
+    res.send("Upload successful!");
 })
 
 app.listen(4000, () => {
